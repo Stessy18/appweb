09ae05f91511be8bc5ea2d73d80a48e35fb1f56a
Stessy18 <francy.keerthi@gmail.com>
2017-04-18 12:02:47 +0530

    db

--- app/src/main/java/com/example/steffy/cfcwebapp/Contact.java
+++ app/src/main/java/com/example/steffy/cfcwebapp/Contact.java
@@ -2,147 +2,177 @@ package com.example.steffy.cfcwebapp;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
 public class Contact extends AppCompatActivity {
 
-    Button subbt, vbt;
-    DatabaseHelper mydb;
-    EditText e1, e2, e3, e4;
+    DatabaseHelper myDb;
+    EditText editname,editmail,editmob,editmsg ;
+    Button btnAddData;
+    Button btnviewAll;
+    Button hbt;
+    Button abt;
+    Button cbt;
+    Button mbt;
 
+    /*Button homebt = (Button) findViewById(R.id.home);
+    Button aboutbt = (Button) findViewById(R.id.about);
+    Button contactbt = (Button) findViewById(R.id.contact);
+    Button mapbt = (Button) findViewById(R.id.map);*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact);
-        Button homebt = (Button) findViewById(R.id.home);
-        Button aboutbt = (Button) findViewById(R.id.about);
-        Button contactbt = (Button) findViewById(R.id.contact);
-       
-        e1 = (EditText) findViewById(R.id.editText3);
-        e2 = (EditText) findViewById(R.id.editText5);
-        e3 = (EditText) findViewById(R.id.editText4);
-        e4 = (EditText) findViewById(R.id.editText6);
+        myDb = new DatabaseHelper(this);
 
-        vbt = (Button) findViewById(R.id.viewbtn);
-        subbt = (Button) findViewById(R.id.submit);
-
-        Button mapbt = (Button) findViewById(R.id.map);
-        final String mess=e4.getText().toString();
+        editname = (EditText)findViewById(R.id.etname);
+        editmail = (EditText)findViewById(R.id.etmail);
+        editmob = (EditText)findViewById(R.id.etphone);
+        editmsg = (EditText)findViewById(R.id.etmsg);
 
 
-        homebt.setOnClickListener(new View.OnClickListener() {
+        btnAddData = (Button)findViewById(R.id.submit);
+        btnviewAll = (Button)findViewById(R.id.viewbtn);
+        mbt=(Button)findViewById(R.id.map);
+        hbt=(Button)findViewById(R.id.home);
+        abt=(Button)findViewById(R.id.about);
+        cbt=(Button)findViewById(R.id.contact);
+
+
+
+      /*  homebt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent gotoIntent = new Intent(Contact.this, MainActivity.class);
                 startActivity(gotoIntent);
             }
         });
-        aboutbt.setOnClickListener(new View.OnClickListener() {
+        abt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent gotoIntent = new Intent(Contact.this, About.class);
                 startActivity(gotoIntent);
             }
         });
-        contactbt.setOnClickListener(new View.OnClickListener() {
+        cbt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent gotoIntent = new Intent(Contact.this, Contact.class);
                 startActivity(gotoIntent);
             }
         });
+
         mapbt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent gotoIntent = new Intent(Contact.this, MapActivity.class);
                 startActivity(gotoIntent);
             }
-        });
-        subbt.setOnClickListener(new View.OnClickListener() {
+        });*/
+        AddData();
+        viewAll();
+        Map();
+
+        Home();
+        Abt();
+        Cont();
+
+    }
+
+    private void Cont() {
+        cbt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-               boolean isInserted = mydb.insertdata(e1.getText().toString(), e2.getText().toString(), e3.getText().toString(), mess);
-
-                if (isInserted == true)
-                    Toast.makeText(Contact.this, "Data Inserted", Toast.LENGTH_LONG).show();
-                else
-                    Toast.makeText(Contact.this, "Data not Inserted", Toast.LENGTH_LONG).show();
-                //sendEmail();
-
+                Intent gotoIntent = new Intent(Contact.this, Contact.class);
+                startActivity(gotoIntent);
             }
         });
-
-
-
-
     }
 
-    protected void sendEmail() {
-        Log.i("Send email", "");
-        String[] TO = {"anithavikkiraman@gmail.com"};
-        String[] CC = {"francy.keerthi@gmail.com"};
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-        emailIntent.putExtra(Intent.EXTRA_CC, CC);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
-
-        try {
-            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-            finish();
-            Log.i("Finished sending email", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(Contact.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void viewres() {
-
-        vbt.setOnClickListener(new View.OnClickListener() {
-            @Override
+    private void Abt() {
+        abt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Cursor res = mydb.getAllData();
-                if (res.getCount() == 0) {
-                    showMessage("Error", "Nothing found");
-                    return;
-                }
-
-                StringBuilder buffer = new StringBuilder();
-                while (res.moveToNext()) {
-                    buffer.append("Name:" + res.getString(0) + "\n");
-                    buffer.append("e-Mail:" + res.getString(1) + "\n");
-                    buffer.append("Phone no.:" + res.getString(2) + "\n");
-                    buffer.append("Message:" + res.getString(3) + "\n");
-                }
-
-                showMessage("Data", buffer.toString());
+                Intent gotoIntent = new Intent(Contact.this, About.class);
+                startActivity(gotoIntent);
             }
         });
-
     }
 
-    private void showMessage(String title, String Message) {
+    private void Home() {
+        hbt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent gotoIntent = new Intent(Contact.this, MainActivity.class);
+                startActivity(gotoIntent);
+            }
+        });
+    }
 
+    private void Map() {
+        mbt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent gotoIntent = new Intent(Contact.this, MapActivity.class);
+                startActivity(gotoIntent);
+            }
+        });
+    }
+
+    public  void AddData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editname.getText().toString(),
+                                editmail.getText().toString(),
+                                editmob.getText().toString(),
+                                editmsg.getText().toString());
+                        if(isInserted == true)
+                            Toast.makeText(Contact.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(Contact.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+    }
+
+    public void viewAll() {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb.getAllData();
+                        if(res.getCount() == 0) {
+                            // show message
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("Id :"+ res.getString(0)+"\n");
+                            buffer.append("Name :"+ res.getString(1)+"\n");
+                            buffer.append("Mail ID :"+ res.getString(2)+"\n");
+                            buffer.append("Mobile No. :"+ res.getString(3)+"\n");
+                            buffer.append("Message :"+ res.getString(4)+"\n\n");
+
+                        }
+
+                        // Show all data
+                        showMessage("Messages",buffer.toString());
+                    }
+
+
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setCancelable(true);
         builder.setTitle(title);
         builder.setMessage(Message);
         builder.show();
-
-
     }
-
-
-
-
-
 }
--- app/src/main/java/com/example/steffy/cfcwebapp/DatabaseHelper.java
+++ app/src/main/java/com/example/steffy/cfcwebapp/DatabaseHelper.java
@@ -7,52 +7,50 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
-
 public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "CFC.db";
+    public static final String TABLE_NAME = "mesg_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "MAILID";
+    public static final String COL_4 = "MOBILENO";
+    public static final String COL_5 = "MSG";
 
-    public static final String DATABASE_NAME="CFC.db";
-    public static final String TABLE_NAME="Details";
-    public static final String col_1="NAME";
-    public static final String col_2="EMAIL";
-    public static final String col_3="PHONENO";
-    public static final String col_4="MSG";
 
     public DatabaseHelper(Context context) {
-        super(context,DATABASE_NAME,null,1);
-
+        super(context, DATABASE_NAME, null, 3);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table"+TABLE_NAME+"(NAME TEXT,EMAIL TEXT,PHONENO INTEGER,MSG TEXT)");
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,MAILID TEXT,MOBILENO INTEGER,MSG TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    db.execSQL("DROP TABLE IF EXISTS"+TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
-    public boolean insertdata(String name,String mail,String cellno,String msg) {
+    public boolean insertData(String name, String mail, String mob,String mess) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        int phno=Integer.parseInt(cellno);
-        contentValues.put(col_1, name);
-        contentValues.put(col_2, mail);
-        contentValues.put(col_3, phno);
-        contentValues.put(col_4, msg);
-        long result=db.insert(TABLE_NAME,null,contentValues);
-        if(result==-1)
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, mail);
+        contentValues.put(COL_4, mob);
+        contentValues.put(COL_5, mess);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1)
             return false;
         else
             return true;
+
     }
 
-    public Cursor getAllData(){
-
-        SQLiteDatabase db =this.getWritableDatabase();
-        Cursor res=db.rawQuery("select * from "+TABLE_NAME,null);
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
         return res;
-
     }
 }
